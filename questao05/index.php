4e2792ca37dba721081078b9ab539516e978c026
Elieudo Maia <elieudo.maia@gmail.com>
2020-02-07 21:16:51 -0300

    Resolução da quetão 5

---
+++ questao05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 04</title>
+</head>
+<body>
+    
+<?php
+
+
+    for($i=0; $i<=100; $i+=2) { // Ou usar o $i%2 == 0
+        echo "$i<br />";
+    }
+
+?>
+
+</body>
+</html>
