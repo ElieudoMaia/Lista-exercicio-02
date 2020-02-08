647fb6437a84c611d45a4abeeff833fe7b40149c
Elieudo Maia <elieudo.maia@gmail.com>
2020-02-07 21:11:30 -0300

    Resolução da quetão 3

---
+++ questao03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 03</title>
+</head>
+<body>
+    
+<?php
+
+    $total = 0;
+
+    for($i=0; $i<=100; $i++) {
+        $total += $i;
+    }
+
+    echo "0 + 1 + 2 + ... + 100 = <strong>$total</strong>";
+
+?>
+
+</body>
+</html>
