a3bce166aded54ea56f2e5b0bb07b17c135aae62
Elieudo Maia <elieudo.maia@gmail.com>
2020-02-07 21:22:20 -0300

    Resolução da quetão 6

---
+++ questao06/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 04</title>
+</head>
+<body>
+    
+<?php
+
+    for($i=500; $i>=10; $i--) {
+        if($i%2 == 0) {
+            echo "$i<br />";
+        }
+    }
+
+?>
+
+</body>
+</html>
