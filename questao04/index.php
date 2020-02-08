ef3757bd822d6090db4d8e69c13755a612975a7f
Elieudo Maia <elieudo.maia@gmail.com>
2020-02-07 21:13:47 -0300

    Resolução da quetão 4

---
+++ questao04/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 04</title>
+</head>
+<body>
+    
+<?php
+
+    for($i=0; $i<=100; $i++) {
+        echo "$i<br />";
+    }
+
+?>
+
+</body>
+</html>
